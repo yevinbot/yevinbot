711faaf5073c7f65f144b197e18256ef8903c8f3
xiwangly2 <1334850101@qq.com>
2023-03-03 12:41:55 +0800

    测试提交

--- main.py
+++ main.py
@@ -1,36 +1,68 @@
-import socket
+import websocket
 import json
-import go
-from read import Read
 
-ListenSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ListenSocket.bind(('127.0.0.1', 5701))
-ListenSocket.listen(100)
-read_i = Read()
-read_i.s_key()
+# 机器人配置信息
+HOST = '127.0.0.1'  # go-cqhttp 服务器地址
+PORT = 6700  # go-cqhttp 服务器端口
+ACCESS_TOKEN = "Bearer " + '114514'  # 访问令牌
 
-HttpResponseHeader = '''HTTP/1.1 200 OK
-Content-Type: text/html\r\n\r\n
-'''
+# 构造 API 请求数据
+def build_api_data(action, params):
+    data = {
+        'action': action,
+        'params': params,
+        'echo': 'hello world'
+    }
+    return json.dumps(data)
 
-def request_to_json(msg):
-    for i in range(len(msg)):
-        if msg[i]=="{" and msg[-1]=="\n":
-            return json.loads(msg[i:])
-    return None
+# 发送 API 请求
+def send_api_request(action, params):
+    ws = websocket.create_connection(f"ws://{HOST}:{PORT}/ws/api/")
+    data = build_api_data(action, params)
+    ws.send(data)
+    response = ws.recv()
+    ws.close()
+    return json.loads(response)
 
-#需要循环执行，返回值为json格式
-def rev_msg():# json or None
-    Client, Address = ListenSocket.accept()
-    Request = Client.recv(1024).decode(encoding='utf-8')
-    rev_json=request_to_json(Request)
-    Client.sendall((HttpResponseHeader).encode(encoding='utf-8'))
-    Client.close()
-    return rev_json
+# 接收消息
+def receive_messages():
+    ws = websocket.create_connection(f"ws://{HOST}:{PORT}/ws/event/")
+    event = {"action": "get_latest_events", "params": {"access_token": ACCESS_TOKEN}}
+    ws.send(json.dumps(event))
+    response = ws.recv()
+    ws.close()
+    messages = json.loads(response)['data']
+    return messages
 
-while True:
-    msg = rev_msg()
-    if msg['post_type'] == 'message':
-        print(f"收到来自群号{msg['group_id']}的消息{msg['message']}")
-        if msg['message'] in read_i.dic_key:
-            go.send(msg['group_id'], read_i.dic_key[msg['message']])
+# 发送消息
+def send_message(message, message_type, target_id):
+    params = {
+        'access_token': ACCESS_TOKEN,
+        'message_type': message_type,
+        'message': message,
+        'user_id': target_id
+    }
+    send_api_request('send_msg', params)
+
+# 运行机器人
+def run_bot():
+    while True:
+        messages = receive_messages()
+        for message in messages:
+            message_type = message['message_type']
+            if message_type == 'private':
+                # 处理私聊消息
+                user_id = message['user_id']
+                message_text = message['message']
+                # TODO: 根据收到的消息内容进行相应处理
+                send_message(f'reply to user {user_id}: {message_text}', 'private', user_id)
+            elif message_type == 'group':
+                # 处理群聊消息
+                group_id = message['group_id']
+                user_id = message['user_id']
+                message_text = message['message']
+                # TODO: 根据收到的消息内容进行相应处理
+                send_message(f'reply to group {group_id}, user {user_id}: {message_text}', 'group', group_id)
+
+if __name__ == '__main__':
+    run_bot()
