7c5bdc8cce575ea828171fefda8cbc732bde1f50
xiwangly2 <1334850101@qq.com>
2023-03-03 14:19:06 +0800

    搞定了接收的部分

--- main.py
+++ main.py
@@ -18,67 +18,70 @@ def build_api_data(action, params):
 
 # 发送 API 请求
 def send_api_request(action, params):
-    ws = websocket.create_connection(f"ws://{HOST}:{PORT}/ws/api/")
+    global ws
     data = build_api_data(action, params)
     ws.send(data)
     response = ws.recv()
-    ws.close()
     return json.loads(response)
 
 # 接收消息
 def receive_messages():
-    ws = websocket.create_connection(f"ws://{HOST}:{PORT}/ws/event/", header=[f"Authorization: Bearer {ACCESS_TOKEN}"])
-    event = {"action": "get_latest_events", "params": {"access_token": ACCESS_TOKEN}}
+    global ws
+    ws = websocket.create_connection(f"ws://{HOST}:{PORT}/", header=[f"Authorization: Bearer {ACCESS_TOKEN}"])
+    event = {"action": "get_login_info", "params": {"access_token": ACCESS_TOKEN}}
     ws.send(json.dumps(event))
-    response = ws.recv()
-    ws.close()
-    messages = json.loads(response)['data']
-    return messages
 
 # 发送消息
 def send_message(message, message_type, target_id):
     params = {
         'access_token': ACCESS_TOKEN,
-        'message_type': message_type,
-        'message': message,
-        'user_id': target_id
+        'message': message
     }
-    send_api_request('send_msg', params)
+    if(message_type == 'group'):
+        params['group_id'] = target_id
+        send_api_request('send_group_msg', params)
+    elif(message_type == 'pravite'):
+        params['user_id'] = target_id
+        send_api_request('send_pravite_msg', params)
 
 # 运行机器人
 def run_bot():
-    try:
-        ws = websocket.create_connection(f"ws://{HOST}:{PORT}/", header=[f"Authorization: Bearer {ACCESS_TOKEN}"])
-        event = {"action": "get_login_info", "params": {"access_token": ACCESS_TOKEN}}
-        ws.send(json.dumps(event))
-    except:
-        print("Error creating websocket connection")
-        exit()
+    global ws
+    receive_messages()
     while True:
-        # messages = receive_messages()
         try:
+            # 接收返回的消息
             response = ws.recv()
         except Exception:
             print("Connection error")
         messages = json.loads(response)
-        print(response)
+        messages.setdefault('post_type', None)
+        messages.setdefault('message_type', None)
+        print(messages)
+                
+        if messages['post_type'] != "message":
+            continue
         
-        continue
-        for message in messages:
-            message_type = message['message_type']
-            if message_type == 'private':
-                # 处理私聊消息
-                user_id = message['user_id']
-                message_text = message['message']
-                # TODO: 根据收到的消息内容进行相应处理
-                send_message(f'reply to user {user_id}: {message_text}', 'private', user_id)
-            elif message_type == 'group':
-                # 处理群聊消息
-                group_id = message['group_id']
-                user_id = message['user_id']
-                message_text = message['message']
-                # TODO: 根据收到的消息内容进行相应处理
-                send_message(f'reply to group {group_id}, user {user_id}: {message_text}', 'group', group_id)
+        message_type = messages['message_type']
+        if message_type == 'private':
+            # 处理私聊消息
+            user_id = messages['user_id']
+            message_text = messages['message']
+            # TODO: 根据收到的消息内容进行相应处理
+            send_message(f'reply to user {user_id}: {message_text}', 'private', user_id)
+        elif message_type == 'group':
+            # 处理群聊消息
+            group_id = messages['group_id']
+            user_id = messages['user_id']
+            message_text = messages['message']
+            # TODO: 根据收到的消息内容进行相应处理
+            send_message(f'reply to group {group_id}, user {user_id}: {message_text}', 'group', group_id)
 
 if __name__ == '__main__':
+    # 尝试建立websocket连接
+    try:
+        ws = websocket.create_connection(f"ws://{HOST}:{PORT}/", header=[f"Authorization: Bearer {ACCESS_TOKEN}"])
+    except:
+        print("Error creating websocket connection")
+        exit()
     run_bot()
