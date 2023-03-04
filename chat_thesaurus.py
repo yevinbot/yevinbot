2533fdde693c1da4d6868e7bcb2038b8bbb7c868
xiwangly2 <1334850101@qq.com>
2023-03-04 17:27:19 +0800

    测试正则表达式

--- chat_thesaurus.py
+++ chat_thesaurus.py
@@ -1,4 +1,8 @@
+import re
+
 def chat_thesaurus(message, message_type = None, target_id = None):
     if message == "/help":
         text = "这是一个帮助列表"
+    elif re.match('http(s)\:\/\/', message):
+        text = "这是一个网址"
     return text
