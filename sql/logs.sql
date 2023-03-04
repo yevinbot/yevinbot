e4bdf1624210318ca02bfaa4f1adb073e296eca3
xiwangly2 <1334850101@qq.com>
2023-03-04 23:56:31 +0800

    提交

--- sql/logs.sql
+++ sql/logs.sql
@@ -11,7 +11,7 @@
  Target Server Version : 50726
  File Encoding         : 65001
 
- Date: 04/03/2023 23:17:33
+ Date: 04/03/2023 23:56:08
 */
 
 SET NAMES utf8mb4;
@@ -23,9 +23,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `logs`;
 CREATE TABLE `logs`  (
   `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
-  `json` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL,
+  `json` json NOT NULL,
   `time` datetime NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB AUTO_INCREMENT = 6 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB AUTO_INCREMENT = 2 CHARACTER SET = utf8mb4 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 SET FOREIGN_KEY_CHECKS = 1;
